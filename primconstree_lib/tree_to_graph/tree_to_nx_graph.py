ce77950fb3b266faf7db7e87af173367159e62b0
harshana20011 <runh1501.usherbrooke.ca>
2024-07-07 22:41:38 -0400

    .

--- primconstree_lib/tree_to_graph/tree_to_nx_graph.py
+++ primconstree_lib/tree_to_graph/tree_to_nx_graph.py
@@ -8,7 +8,3 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # Add the parent directory to the system path
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
-
-from tree.rename_set_of_trees_avg_branch_lengths import (
-    rename_set_of_trees_compute_avg_branch_length,
-)
